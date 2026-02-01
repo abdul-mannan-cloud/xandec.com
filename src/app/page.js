08706815e9c7a421ed5e25d76889f89f653e8c74
Khizer <m.khizerr01@gmail.com>
2026-02-01 21:37:23 +0300

    minor fixes

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function HomePage() {
       <div className="snap-section">
         <HeroSection />
       </div>
-
       <div className="snap-section">
         <ServicesSection />
       </div>
